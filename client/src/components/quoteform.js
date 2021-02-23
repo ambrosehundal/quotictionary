e0876902dd76d4d0c864aea7a9eb2f6815d25ad5
Ambrose Hundal <amshunda@ucsc.edu>
2021-02-22 23:16:27 -0800

    added function to create new quotes in form component

---
+++ client/src/components/quoteform.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+
+
+const quoteForm = () => {
+    const [quote, setQuote] = useState("")
+
+
+
+
+    return (
+        <form onSubmit={submitquote}>
+
+        
+        
+        
+        
+        
+        
+        </form>
+
+
+
+
+    )
+}
+
+
+export default quoteform;
+
